68cd27507fcf4985f8990437e4cb298167d57d28
kvandertoorn <k.vandertoorn@student.tudelft.nl>
2024-09-10 23:39:49 +0200

    Skillset

--- src/pages/Home/Skills/SkillSet.jsx
+++ src/pages/Home/Skills/SkillSet.jsx
@@ -28,9 +28,7 @@ const SkillItemIcon = styled.div`
   top: 0.0775em;
   width: 1.05em;
   height: auto;
-  /* &:hover {
-    color: ${({ color }) => color || 'inherit'};
-  } */
+  
   & > svg {
     width: 1.05em;
     height: auto;
@@ -43,6 +41,10 @@ const SkillItemName = styled(SectionText)`
 
   @media (prefers-color-scheme: dark) {
     color: ${({ theme }) => theme.colors.dark.primaryText}; // Dark mode skill item color
+  }
+
+  &::selection {
+    background-color: ${({ color }) => color || 'inherit'}55;
   }
 `;
 
@@ -58,8 +60,8 @@ const SkillSet = ({ title, children }) => (
 // Skill component for individual skills
 export const Skill = ({ icon: Icon, iconColor, children }) => (
   <SkillItem>
-    <SkillItemIcon color={iconColor}>{Icon ? <Icon /> : null}</SkillItemIcon>
-    <SkillItemName as="li">{children}</SkillItemName>
+    <SkillItemIcon >{Icon ? <Icon /> : null}</SkillItemIcon>
+    <SkillItemName as="li" color={iconColor}>{children}</SkillItemName>
   </SkillItem>
 );
 
