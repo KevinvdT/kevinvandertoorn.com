17054972e28ffbfd4224b17b7d60e1be4fe8291a
kvandertoorn <kevinvdtoorn@live.nl>
2024-11-24 02:03:50 +0100

    Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import smoothscroll from 'smoothscroll-polyfill';
 import store from './redux/store';
 import App from './App.jsx';
 import './i18n';  // Import the i18n configuration
+import ReactGA from 'react-ga4'; // Import react-ga4
+
+// Initialize Google Analytics
+ReactGA.initialize('G-6K881PRR63'); // Replace with your actual Measurement ID
+ReactGA.send('pageview'); // Send initial pageview
 
 // Polyfill for smooth scrolling
 smoothscroll.polyfill();
